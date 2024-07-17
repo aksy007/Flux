6f959ea67f6a7e5d36f4415272415de3758cb6ea
Aksy <44296256+aksy007@users.noreply.github.com>
2024-07-17 22:30:28 +0530

    Add Flux Sidebar (#2)
    * Add Initial Sidebar changes
    * add common Dropdown Select Method
    * Add Sidebar

---
+++ src/common/Select/SelectData.tsx
@@ -0,0 +1,38 @@
+export type DropdownOption = {
+	id: number;
+	name: string;
+	owner: string;
+	// Add other properties as needed
+};
+
+export interface SelectDropdownProps<T> {
+	DropdownList: T[];
+	DropdownLabel: string;
+	customClass?: string;
+}
+
+export const selectStyles = {
+	minWidth: 120,
+	borderRadius: 1,
+	'& .MuiInputLabel-root': { 
+	  color: 'white', 
+	  '&.Mui-focused': {
+		color: 'white',
+	  }
+	},
+	'& .MuiOutlinedInput-root': {
+	  color: 'white',
+	  '& fieldset': {
+		borderColor: 'white',
+	  },
+	  '&:hover fieldset': {
+		borderColor: 'white',
+	  },
+	  '&.Mui-focused fieldset': {
+		borderColor: 'white',
+	  },
+	},
+	'& .MuiSelect-icon': {
+	  color: 'white',
+	}
+};
---
+++ src/components/Sidebar/SidebarData.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import GroupsIcon from '@mui/icons-material/Groups';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+
+const ORGANISATION_NAME: string = 'Monarch';
+
+export interface ProjectListType {
+	name: string;
+	owner: string;
+	id: number;
+}
+
+export interface UserOptionType {
+	icon: React.FC,
+	option: string,
+	link: string
+
+}
+
+const InitialProject: ProjectListType = {
+	name: '',
+	owner: '',
+	id: 0,
+};
+
+const UserOptions: UserOptionType[] = [
+	{
+	  icon: AssignmentIcon, 
+	  option: "Issues",
+	  link: "/issues",
+	},
+	{
+	  icon: GroupsIcon,
+	  option: "Team",
+	  link: "/team", 
+	},
+	{
+	  icon: RocketLaunchIcon, 
+	  option: "Releases",
+	  link: "/releases",
+	},
+  ];
+
+const ProjectList: ProjectListType[] = [
+	{
+		name: 'FLUX',
+		owner: 'AksyK',
+		id: 1,
+	},
+	{
+		name: 'CRM',
+		owner: 'AksyK',
+		id: 2,
+	},
+];
+
+export { ORGANISATION_NAME, InitialProject, ProjectList, UserOptions };
