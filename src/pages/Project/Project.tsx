609803952351f2a4c40daf4dbfa2bfda7de7d526
aksy007 <akshaykolhe04@gmail.com>
2024-09-28 15:37:22 +0530

    update component fetch logic

---
+++ src/pages/Project/Project.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ProjectPageProps {}
+
+const Project: React.FC<ProjectPageProps> = () => {
+    return (
+        <div> Project Page </div>
+    );
+};
+
+export default Project;
--- src/router/route.tsx
+++ src/router/route.tsx
@@ -3,21 +3,23 @@ import { Loader } from "../components/Loader";
 import About from "../components/About";
 import { NotFound } from "../components/404";
 import LandingPage from "../pages/Landing/Landing";
+import Project from "../pages/Project/Project";
 
 const routeComponentMapping: { [key: string]: React.ComponentType } = {
   "/": LandingPage,
-  "/about": About,
+  "/project": Project,
   "/*": Loader
 };
 
 // Function to render the component based on the pathname
 export const renderComponent = (pathname: string): React.ReactElement => {
   try {
-    const Component = routeComponentMapping?.[pathname] || null;
+    const pageUrl = pathname?.split('/');
+    const formatedPathname = pageUrl?.[0] === '' ? `/${pageUrl[1]}` :  pathname;
+    const Component = routeComponentMapping?.[formatedPathname] || null;
     return Component ? <Component /> : <Loader />;
   } catch (error) {
-    console.log("err: ", error);
-    return <NotFound />;
+      return <NotFound />;
   }
 };
 
